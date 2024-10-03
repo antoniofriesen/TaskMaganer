8bed765a88d3e311323bc82d7999e28f1c75a591
Antonio Friesen <91128531+antoniofriesen@users.noreply.github.com>
2024-10-03 17:52:40 +0200

    connection DB
    set up connection to MongoDB;
    created .env

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const APP = express();
 const tasks = require("./routes/tasks.js");
+const connectDB = require("./db/connect.js");
+require("dotenv").config();
 
 // middleware
 APP.use(express.json());
@@ -14,4 +16,14 @@ APP.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-APP.listen(port, console.log(`Server is listening on port ${port} ...`));
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        console.log("CONNECTED TO DB!");
+        APP.listen(port, console.log(`Server is listening on port ${port} ...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,7 @@
-const connectionString = "mongodb+srv://antonio:<db_password>@nodejsprojects.zeejm.mongodb.net/?retryWrites=true&w=majority&appName=NodeJsProjects";
+const mongoose = require("mongoose");
+
+const connectDB = (url) => {
+    return mongoose.connect(url)
+};
+
+module.exports = connectDB;
