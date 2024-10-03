a24568584444faa18f8c1d55096c0333dab0e323
Antonio Friesen <91128531+antoniofriesen@users.noreply.github.com>
2024-10-03 18:52:07 +0200

    logic
    added logic to CRUD operations

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ const port = 3000;
 
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URI);
+        await connectDB(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log("CONNECTED TO DB!");
         APP.listen(port, console.log(`Server is listening on port ${port} ...`));
     } catch (error) {
